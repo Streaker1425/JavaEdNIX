bc732a48d40b0da895f48cb2cea45d62e98bb361
StreakerOne <i.o.tkachov@student.khai.edu>
2021-11-07 21:06:13 +0200

    Constant values class added, codestyle fixes

---
+++ src/feature/coffeMachine/CoffeeValues.java
@@ -0,0 +1,27 @@
+package feature.coffeMachine;
+
+public class Values {
+
+    public static final int WATER_CAPACITY = 50000;
+    public static final int MILK_CAPACITY = 15000;
+    public static final int COFFEE_CAPACITY = 5000;
+    public static final int CUPS_CAPACITY = 150;
+
+    public static final int ESPRESSO_WATER = 250;
+    public static final int ESPRESSO_COFFEE = 16;
+    public static final int ESPRESSO_PRICE = 4;
+    public static final int ESPRESSO_OPTION = 0;
+
+    public static final int LATTE_WATER = 350;
+    public static final int LATTE_MILK = 75;
+    public static final int LATTE_COFFEE = 20;
+    public static final int LATTE_PRICE = 7;
+    public static final int LATTE_OPTION = 1;
+
+    public static final int CAPPUCCINO_WATER = 200;
+    public static final int CAPPUCCINO_MILK = 100;
+    public static final int CAPPUCCINO_COFFEE = 12;
+    public static final int CAPPUCINO_PRICE = 6;
+    public static final int CAPPUCCINO_OPTION = 2;
+
+}
